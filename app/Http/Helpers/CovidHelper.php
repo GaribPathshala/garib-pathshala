c26d91204974f78f61ec6d39679197021ba16320
Aniket_IN <aniket.das.in@gmail.com>
2021-09-09 12:47:25 +0530

    User Agent Added

--- app/Http/Helpers/CovidHelper.php
+++ app/Http/Helpers/CovidHelper.php
@@ -5,6 +5,35 @@ namespace App\Http\Helpers;
 
 class CovidHelper{
 
+    
+    static public function fetchStates() {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://cdn-api.co-vin.in/api/v2/admin/location/states',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'accept: application/json',
+            'Accept-Language: hi_IN',
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        dd($response);
+        $response = json_decode($response);
+        return $response;
+    }
+    
+
     static public function verifyOtp($data)
     {
         $otp    = hash("sha256", $data['otp']);
@@ -32,13 +61,15 @@ class CovidHelper{
         CURLOPT_HTTPHEADER => array(
             'accept: application/json',
             'x-api-key: 3sjOr2rmM52GzhpMHjDEE1kpQeRxwFDr4YcBEimi',
-            'Content-Type: application/json'
+            'Content-Type: application/json',
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
         ),
         ));
 
         $response = curl_exec($curl);
 
         curl_close($curl);
+
         $response = json_decode($response);
         return $response;
     }
@@ -67,7 +98,8 @@ class CovidHelper{
         CURLOPT_HTTPHEADER => array(
             'accept: application/json',
             'x-api-key: 3sjOr2rmM52GzhpMHjDEE1kpQeRxwFDr4YcBEimi',
-            'Content-Type: application/json'
+            'Content-Type: application/json',
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
         ),
         ));
       
@@ -94,7 +126,8 @@ class CovidHelper{
         CURLOPT_CUSTOMREQUEST => 'GET',
         CURLOPT_HTTPHEADER => array(
             'accept: application/json',
-            'Accept-Language: hi_IN'
+            'Accept-Language: hi_IN',
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
         ),
         ));
 
@@ -105,33 +138,6 @@ class CovidHelper{
         return $response;
     }
 
-
-    static public function fetchStates() {
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://cdn-api.co-vin.in/api/v2/admin/location/states',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
-        CURLOPT_HTTPHEADER => array(
-            'accept: application/json',
-            'Accept-Language: hi_IN'
-        ),
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-
-        $response = json_decode($response);
-        return $response;
-    }
-    
     
     static public function calenderByDistrictId($data) {
         $curl = curl_init();
@@ -148,7 +154,8 @@ class CovidHelper{
         CURLOPT_CUSTOMREQUEST => 'GET',
         CURLOPT_HTTPHEADER => array(
             'accept: application/json',
-            'Accept-Language: hi_IN'
+            'Accept-Language: hi_IN',
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
         ),
         ));
 
@@ -175,7 +182,8 @@ class CovidHelper{
         CURLOPT_CUSTOMREQUEST => 'GET',
         CURLOPT_HTTPHEADER => array(
             'accept: application/json',
-            'Accept-Language: hi_IN'
+            'Accept-Language: hi_IN',
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
         ),
         ));
 
@@ -203,7 +211,8 @@ class CovidHelper{
         CURLOPT_CUSTOMREQUEST => 'GET',
         CURLOPT_HTTPHEADER => array(
             'accept: application/json',
-            'Accept-Language: hi_IN'
+            'Accept-Language: hi_IN',
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
         ),
         ));
 
